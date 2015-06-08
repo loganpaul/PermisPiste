f149fc87b4760bc7e9ebfa30b6f8c128f4b02a40
Laura <premillieu.laura@gmail.com>
2015-06-08 10:03:56 +0200

    configuration

---
+++ src/main/java/com/epul/permisPiste/AppConfig.java
@@ -0,0 +1,9 @@
+package com.epul.permisPiste;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.epul.permisPiste.*")
+public class AppConfig {
+}
--- src/main/java/com/epul/permisPiste/WebAppInitializer.java
+++ src/main/java/com/epul/permisPiste/WebAppInitializer.java
@@ -1,65 +1,61 @@
 package com.epul.permisPiste;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
 public class WebAppInitializer implements WebApplicationInitializer {
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		WebApplicationContext context = getContext();
-		servletContext.addListener(new ContextLoaderListener(context));
-		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.setConfigLocation("com.epul.permisPiste.*");
+		
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/");
+		
+		servletContext.addListener(new ContextLoaderListener(context));
 	}
 
-	private AnnotationConfigWebApplicationContext getContext() {
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-		context.setConfigLocation("/WEB-INF/spring/root-context.xml");
-		return context;
-	}
+//	private AnnotationConfigWebApplicationContext getContext() {
+//		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+//		context.register(ApplicationContext.class);
+//		// context.setConfigLocation("/WEB-INF/spring/root-context.xml");
+//		return context;
+//	}
 
 }
 /*
-<?xml version="1.0" encoding="UTF-8"?>
-<web-app version="2.5" xmlns="http://java.sun.com/xml/ns/javaee"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xsi:schemaLocation="http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd">
-
-	<!-- The definition of the Root Spring Container shared by all Servlets and Filters -->
-	<context-param>
-		<param-name>contextConfigLocation</param-name>
-		<param-value>/WEB-INF/spring/root-context.xml</param-value>
-	</context-param>
-	
-	<!-- Creates the Spring Container shared by all Servlets and Filters -->
-	<listener>
-		<listener-class>org.springframework.web.context.ContextLoaderListener</listener-class>
-	</listener>
-
-	<!-- Processes application requests -->
-	<servlet>
-		<servlet-name>appServlet</servlet-name>
-		<servlet-class>org.springframework.web.servlet.DispatcherServlet</servlet-class>
-		<init-param>
-			<param-name>contextConfigLocation</param-name>
-			<param-value>/WEB-INF/spring/appServlet/servlet-context.xml</param-value>
-		</init-param>
-		<load-on-startup>1</load-on-startup>
-	</servlet>
-		
-	<servlet-mapping>
-		<servlet-name>appServlet</servlet-name>
-		<url-pattern>/</url-pattern>
-	</servlet-mapping>
-
-</web-app>
-*/
+ * <?xml version="1.0" encoding="UTF-8"?> <web-app version="2.5"
+ * xmlns="http://java.sun.com/xml/ns/javaee"
+ * xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation=
+ * "http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd"
+ * >
+ * 
+ * <!-- The definition of the Root Spring Container shared by all Servlets and
+ * Filters --> <context-param> <param-name>contextConfigLocation</param-name>
+ * <param-value>/WEB-INF/spring/root-context.xml</param-value> </context-param>
+ * 
+ * <!-- Creates the Spring Container shared by all Servlets and Filters -->
+ * <listener>
+ * <listener-class>org.springframework.web.context.ContextLoaderListener
+ * </listener-class> </listener>
+ * 
+ * <!-- Processes application requests --> <servlet>
+ * <servlet-name>appServlet</servlet-name>
+ * <servlet-class>org.springframework.web
+ * .servlet.DispatcherServlet</servlet-class> <init-param>
+ * <param-name>contextConfigLocation</param-name>
+ * <param-value>/WEB-INF/spring/appServlet/servlet-context.xml</param-value>
+ * </init-param> <load-on-startup>1</load-on-startup> </servlet>
+ * 
+ * <servlet-mapping> <servlet-name>appServlet</servlet-name>
+ * <url-pattern>/</url-pattern> </servlet-mapping>
+ * 
+ * </web-app>
+ */
